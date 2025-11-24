6a804f85008781e49bc5ff872f58fc11cf7a46a5
alexsander ribeiro <102040731+Sander0026@users.noreply.github.com>
2025-11-24 20:14:46 -0300

    adicionado o arquivo worker_email.py para gerenciar o envio de emails via fila

---
+++ workers/worker_email.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import time
+from multiprocessing.managers import BaseManager
+
+# --- Configuração de Path ---
+# Adiciona o diretório raiz do projeto ao path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(script_dir, '..'))
+sys.path.append(project_root)
+# --- Fim da Configuração de Path ---
+
+# Importa a lambda específica de email e as configs de fila
+try:
+    from src.lambdas import lambda_enviador_email
+    
+    # Importa as configs de conexão E o nome da fila EMAIL
+    from filas.queue_manager import (
+        QUEUE_MANAGER_PORT, 
+        QUEUE_MANAGER_AUTHKEY, 
+        FILA_EMAIL  # Atenção: Fila diferente!
+    )
+except ImportError:
+    print("Erro: Não foi possível encontrar os módulos 'src'.")
+    print("Verifique a estrutura de pastas e o 'queue_manager.py'.")
+    sys.exit(1)
+
+
+def conectar_ao_gerenciador_de_filas():
+    """
+    Tenta se conectar ao Manager de filas.
+    Este código é IDÊNTICO ao dos outros workers.
+    """
+    print("[WORKER_EMAIL] Conectando ao servidor de filas (localhost:50001)...")
+    
+    # Registra o nome da fila que este worker precisa
+    BaseManager.register(FILA_EMAIL) 
+    
+    manager_cliente = BaseManager(
+        address=('127.0.0.1', QUEUE_MANAGER_PORT), 
+        authkey=QUEUE_MANAGER_AUTHKEY
+    )
+    
+    while True:
+        try:
+            manager_cliente.connect()
+            print("[WORKER_EMAIL] 🚀 Conectado ao servidor de filas!")
+            return manager_cliente
+        except ConnectionRefusedError:
+            print("[WORKER_EMAIL] Conexão recusada. Tentando novamente em 5s...")
+            time.sleep(5)
+
+def iniciar_worker():
+    """
+    Função principal do worker. Fica em loop infinito escutando a fila de EMAIL.
+    """
+    manager = conectar_ao_gerenciador_de_filas()
+    
+    # Pega o objeto da fila 'fila_notificacao_email'
+    fila_email = getattr(manager, FILA_EMAIL)() 
+    
+    print(f"[WORKER_EMAIL] Aguardando emails na fila '{FILA_EMAIL}'...")
+
+    while True:
+        try:
+            # .get() bloqueia e "dorme" até uma mensagem chegar
+            mensagem = fila_email.get()
+            
+            print(f"\n[WORKER_EMAIL] 📥 Nova mensagem de email recebida: {mensagem}")
+            print(f"[WORKER_EMAIL] ⚙️ Iniciando 'lambda_enviador_email'...")
+            
+            # Executa a Lambda de envio de email (que apenas simula com um print)
+            lambda_enviador_email.enviar(mensagem)
+            
+            print(f"[WORKER_EMAIL] ✅ Email enviado (simulado).")
+            print(f"[WORKER_EMAIL] Aguardando novas mensagens...")
+
+        except (KeyboardInterrupt, SystemExit):
+            print("[WORKEM_EMAIL] Desligando...")
+            break
+        except Exception as e:
+            print(f"[WORKER_EMAIL] ❌ ERRO ao processar email {mensagem}: {e}")
+
+if __name__ == "__main__":
+    iniciar_worker()
